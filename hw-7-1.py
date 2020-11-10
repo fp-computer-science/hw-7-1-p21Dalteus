9212d79902f42f0b91e968c8a9f8fd47c438f4aa
Darick <p21dalteus@fairfieldprep.org>
2020-11-09 20:55:43 -0500

    hw

---
+++ hw-7-1.py
@@ -0,0 +1,31 @@
+# Author: DA 11/9/2020
+
+# 1
+print('Python'[2])
+
+# 2
+print("Strings are simply a sequence of characters"[30])
+
+# 3
+print(len("excellent"))
+
+# 4
+print('apple' in 'Pineapple')
+
+# Question 5
+print("pear, Peach".endswith("peach"))
+
+# Question 6
+print("x" * 100)
+
+# Question 7
+print(str(54321))
+
+# Question 8
+print("Pineapple".startswith('p'))
+
+# Question 9
+print(' "Livin' + "'" + ' On a Prayer" is my friend' + "'" + 's favorite song. ')
+
+# Question 10
+print(len(' "Livin' + "'" + ' On a Prayer" is my friend' + "'" + 's favorite song. '))
